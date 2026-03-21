f39ec1672acbb4d1d2ce08b89afa2e279c0eaa4c
Ala-Ganesh <alaganeshyadav@gmail.com>
2026-03-21 18:19:05 +0530

    Fixed port binding and gunicorn config

--- app.py
+++ app.py
@@ -39,4 +39,5 @@ app = create_app()
 # Run Server
 # -----------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
